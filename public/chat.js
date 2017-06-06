88b8bf8193860e4f14b8974d26990fd11d80038c
Holt Skinner <HoltWasHere@gmail.com>
2017-06-05 20:29:03 -0700

    Added Typing Notifications

--- public/chat.js
+++ public/chat.js
@@ -5,10 +5,10 @@ var socket = io.connect("http://localhost:4000");
 var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
-    output = document.getElementById('output');
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
 
 // emit events
-
 btn.addEventListener('click', function () {
     socket.emit('chat', {
         message: message.value,
@@ -16,7 +16,17 @@ btn.addEventListener('click', function () {
     });
 });
 
+message.addEventListener('keypress', function () {
+    socket.emit('typing', handle.value);
+});
+
 // listen for events
 socket.on('chat', function (data) {
+    feedback.innerHTML = "";
+
     output.innerHTML += '<p><strong>@' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function (data) {
+    feedback.innerHTML = '<p><em>' + data + 'is typing a message...</em></p>';
 });
